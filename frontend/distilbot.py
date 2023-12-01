b54afc4dd3e1cf11eb83cbfbd8ab22b562511920
skes-hc5 <skes2525@gmail.com>
2023-12-01 20:44:19 +0800

    [feat] call api for model usage

--- frontend/distilbot.py
+++ frontend/distilbot.py
@@ -1,24 +1,58 @@
 import gradio as gr
 import random
 import time
+from transformers import pipeline
+import requests
+API_URL = "https://api-inference.huggingface.co/models/textattack/distilbert-base-uncased-CoLA"
+headers = {"Authorization": "Bearer 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"}
+
+def query1(payload):
+    start = time.time()
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json(), time.time()-start
+
+output = query1({
+	"inputs": "I like you. I love you",
+})
 
 def demo_func_distil_label(input):
-    start_distil = time.time()
-    if input == 'good':
-        return 'POSITIVE', time.time()-start_distil
-    elif input == 'bad':
-        return 'NEGATIVE', time.time()-start_distil
-    else:
-        return random.choice(['POSITIVE', 'NEGATIVE']), time.time()-start_distil
+    output, duration = query1({
+	    "inputs": str(input),
+    })
+    print(f'output = {output}')
+    score = 0
+    for item in output[0]:
+         if item['score'] > score:
+              result = item['label']
+              score = item['score']
+
+    return result, duration
     
+
+
+API_URL_BERT = "https://api-inference.huggingface.co/models/finiteautomata/bertweet-base-sentiment-analysis"
+def query2(payload):
+    start = time.time()
+    response = requests.post(API_URL_BERT, headers=headers, json=payload)
+    return response.json(), time.time()-start
+	
 def demo_func_bert_label(input):
-    start_bert = time.time()
-    if input == 'good':
-        return 'POSITIVE', time.time()-start_bert
-    elif input == 'bad':
-        return 'NEGATIVE', time.time()-start_bert
-    else:
-        return random.choice(['POSITIVE', 'NEGATIVE']), time.time()-start_bert
+    output, duration = query2({
+	    "inputs": str(input),
+    })
+    print(f'output = {output}')
+    score = 0
+    for item in output[0]:
+         if item['score'] > score:
+              result = item['label']
+              score = item['score']
+
+    return result, duration
+    
+
+model_name = "distilbert-base-uncased"
+classifier = pipeline('sentiment-analysis', model=model_name)
+
 
 with gr.Blocks() as distilbot:
     input_text = gr.Textbox()
@@ -33,6 +67,7 @@ with gr.Blocks() as distilbot:
     input_text.submit(demo_func_distil_label, 
                       inputs = input_text, 
                       outputs = [distil_output_label, distil_time_text])
+    print(input_text)
     
     input_text.submit(demo_func_bert_label, 
                       inputs = input_text, 
